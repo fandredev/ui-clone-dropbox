d658efd117f103a4667c13f3eea5eb6b8abc92e8
Felipe <profissionalf.andre@gmail.com>
2020-08-13 20:51:00 -0300

    feat: add section

---
+++ dropbox/src/components/Section/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Container, Content, HeaderWrapper, Header, DropboxLogo } from './styles'
+interface Props {
+  variant: 'blue' | 'beige' | 'white' | 'black';
+  title: string,
+  description: string;
+}
+
+const Section: React.FC<Props> = ({ variant, title, description }) => {
+  const buttonVariant = Math.round(Math.random()); // 0 ou 1
+  return (
+    <Container className={variant}>
+      <HeaderWrapper>
+        <Header>
+          <h1>
+            <DropboxLogo></DropboxLogo>
+            <span>Dropbox</span>
+          </h1>
+          <button>{buttonVariant === 0 ? 'Acessar' : 'Interagir'}</button>
+        </Header>
+      </HeaderWrapper>
+      <Content>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </Content>
+    </Container>
+  )
+}
+
+export default Section;
